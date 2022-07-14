b14d44c62ee84d01cbeea18664bb2af70268db1c
Jamie Ridding <jamie.ridding@hotmail.co.uk>
2022-07-14 12:45:22 +0100

    Remove ?Sized bound from SnowcatSignalVecExt impl

--- src-signals/src/signal_vec.rs
+++ src-signals/src/signal_vec.rs
@@ -4,19 +4,17 @@ use futures_signals::signal_vec::{SignalVec, VecDiff};
 use std::fmt::Debug;
 use std::task::Poll;
 
-pub trait SnowcatSignalVecExt: SignalVec {
+pub trait SnowcatSignalVecExt: SignalVec + Sized {
 	fn group_by_key<Key, KeyFn>(self, key_fn: KeyFn) -> group_by_key::GroupByKey<Key, KeyFn, Self>
 	where Key: Eq + Debug + Clone,
 	      KeyFn: Fn(&Self::Item) -> Key,
-		  Self: Sized,
 		  Self::Item: Debug + Clone,
 	{
 		group_by_key::GroupByKey::new(self, key_fn)
 	}
 }
 
-// FIXME: Why is this ?Sized
-impl<T: ?Sized> SnowcatSignalVecExt for T where T: SignalVec {}
+impl<T> SnowcatSignalVecExt for T where T: SignalVec + Sized {}
 
 pub(crate) fn wrap_poll_result<T>(op: VecDiff<T>) -> Poll<Option<VecDiff<T>>> {
 	Poll::Ready(Some(op))
